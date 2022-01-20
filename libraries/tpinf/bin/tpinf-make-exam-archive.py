cc7865c0603e0ea2a345d0a4a54258ce81310bfe
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2022-01-20 13:15:08 +0100

    updt

---
+++ libraries/tpinf/bin/tpinf-make-exam-archive.py
@@ -0,0 +1,8 @@
+import tpinf
+import sys
+
+dirname = 'exam-subject'
+tpinf.workspace.create_makefile(dirname)
+
+
+
--- libraries/tpinf/setup.py
+++ libraries/tpinf/setup.py
@@ -6,6 +6,6 @@ setuptools.setup(
     version = "1.0",
     scripts = ['bin/tpinf-make-testable-source.py',
                'bin/tpinf-make-subject-source.py',
-               'bin/tpinf-init-subject-workspace.py'],
+               'bin/tpinf-make-exam-archive.py'],
     packages = setuptools.find_packages(),
 )
