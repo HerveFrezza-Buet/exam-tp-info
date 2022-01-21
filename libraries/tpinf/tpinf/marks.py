23d61fbebafe081b38e3fab54712aeec1bbf4f76
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2022-01-21 16:00:38 +0100

    updt

--- libraries/tpinf/tpinf/marks.py
+++ libraries/tpinf/tpinf/marks.py
@@ -79,24 +79,28 @@ def make_report(all_tags, all_students):
     formats = {'header': workbook.add_format({'bg_color' : '#eeeeee', 'bold' : True, 'align' : 'center'}),
                'path'  : workbook.add_format({'align' : 'left'}),
                'name'  : workbook.add_format({'bg_color' : '#555555', 'color' : '#ffffff', 'bold' : True, 'align' : 'left'}),
-               'mark'  : workbook.add_format({'num_format' : '0.0', 'align' : 'right'})}
+               'mark'  : workbook.add_format({'num_format' : '0.0', 'align' : 'right'}),
+               'sum'  : workbook.add_format({'num_format' : '0.0', 'bold' : True, 'align' : 'right'})}
     col_path = 1
     col_name = col_path+1
     col_first_mark = col_name + 1
     col_marks = {mark : col+col_first_mark for col, mark in enumerate(all_tags)}
+    col_last_mark = col_first_mark + len(col_marks) - 1
     col_sum = col_first_mark + len(col_marks) + 1
     line = 2
     
     for m, c in col_marks.items():
         worksheet.write(line, c, m, formats['header'])
+    worksheet.write(line, col_sum, 'Total', formats['header'])
     line += 1
-    
+
     for marks in all_students:
         worksheet.write(line, col_path, str(marks.path), formats['path'])
         worksheet.write(line, col_name, marks.name,      formats['name'])
         for tag, mark in marks.marks.items():
             worksheet.write(line, col_marks[tag], mark,  formats['mark'])
-            
+        formula = '=SUM({})'.format(xlsxwriter.utility.xl_range(line, col_first_mark, line, col_last_mark))
+        worksheet.write_formula(line, col_sum, formula,  formats['sum'])
         line += 1
 
 
