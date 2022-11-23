2123801acfff5354b174600f1847de1ef24540d8
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2022-11-23 11:29:58 +0100

    updt

--- libraries/tpinf/bin/tpinf-make-report.py
+++ libraries/tpinf/bin/tpinf-make-report.py
@@ -10,4 +10,9 @@ all_tags = set()
 all_students = []
 tpinf.marks.collect(Path(sys.argv[1]), all_tags, all_students)
 all_students.sort(key = lambda x : x.name)
+
+# A set should be sorted... I do not understant why I need this.
+all_tags = list(all_tags)
+all_tags.sort()
+
 tpinf.marks.make_report(all_tags, all_students)
